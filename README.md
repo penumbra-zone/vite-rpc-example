f7bb1eb29dba32b3b5a9a9f4e96247f3ecf71322
Gabe Rodriguez <grod220@gmail.com>
2024-10-08 13:49:46 +0200

    Boilerplate

---
+++ README.md
@@ -0,0 +1,12 @@
+# React + TypeScript + Vite
+
+## Guide to connecting to making rpc requests to penumbra nodes
+
+1. Install deps:
+```bash
+npm install @connectrpc/connect-web @connectrpc/connect @penumbra-zone/protobuf
+```
+
+If you are looking to develop an app that connects to Prax go here
+
+https://github.com/penumbra-zone/web/tree/main/packages/client
---
+++ src/clients.ts
@@ -0,0 +1,11 @@
+import { createGrpcWebTransport } from '@connectrpc/connect-web';
+import { createPromiseClient } from '@connectrpc/connect';
+import { TendermintProxyService } from '@penumbra-zone/protobuf';
+
+const GRPC_ENDPOINT = 'https://penumbra.stakewith.binary.builders'
+
+const transport = createGrpcWebTransport({
+  baseUrl: GRPC_ENDPOINT,
+});
+
+export const tendermintClient = createPromiseClient(TendermintProxyService, transport);
